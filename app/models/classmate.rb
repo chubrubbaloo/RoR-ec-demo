83c2411869b1820dc000a6d14a695338defda2d4
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-21 19:39:53 +0100

    Updated validation on email regex.

--- app/models/classmate.rb
+++ app/models/classmate.rb
@@ -2,7 +2,7 @@ class Classmate < ApplicationRecord
   validates :user_id, presence: true
   validates :first_name, :last_name, :email, :phone, presence: true
 
-  validates :email, format: {with: Devise.email_regexp , message: 'only allows real emails with @'}
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i , message: 'only allows real emails with @'}
 
   validates :phone, format: { with: /([6-9]|1[0-3])/,
                                     message: "only allows numbers with a length between 6 and 13" }
