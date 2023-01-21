cc0e45cbdd76229e35bb069a0a8e128321bcea80
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-21 17:41:21 +0100

    Styled School group table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
 
+
+
   has_many :classmates
 end
 
