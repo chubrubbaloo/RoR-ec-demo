57765fff02d630dad3bf85215ddc271017f30d4d
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-21 18:00:35 +0100

    Validation for phone and email.

--- app/models/classmate.rb
+++ app/models/classmate.rb
@@ -1,6 +1,11 @@
 class Classmate < ApplicationRecord
   validates :user_id, presence: true
   validates :first_name, :last_name, :email, :phone, presence: true
+
+  validates :email, format: {with: Devise.email_regexp , message: 'only allows real emails with @'}
+
+  validates :phone, format: { with: /([6-9]|1[0-3])/,
+                                    message: "only allows numbers with a length between 6 and 13" }
   # validates :email, :phone, uniqueness: true
 
   belongs_to :user
