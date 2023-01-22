a8baa16a555c7cda811aa2b81cfe28e5f892fa82
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-22 15:03:19 +0100

    More validation for unique grade for real this time!!!

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,7 +2,7 @@ class Grade < ApplicationRecord
   validates :user_id, presence: true
   validates :code, :course, :points, :grade, presence: true
 
-  validates :code, :course, uniqueness: true
+  validates_uniqueness_of :code, :course, scope: :user_id
 
   validates :grade, format: {with: /(?<=^|[\s,])(?:[A-D][-+]?|F)(?=[-+.]\B|[\s,]|$)/i , message: 'only allow american grades (A,B,C,D,F) '}
 
