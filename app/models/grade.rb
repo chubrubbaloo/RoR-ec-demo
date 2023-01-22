ff6fb6304c3ba6e9d371d68c484ec1e712fab827
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-22 12:24:12 +0100

    Validation for grade.

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,8 @@
 class Grade < ApplicationRecord
+  validates :user_id, presence: true
+  validates :code, :course, :points, :grade, presence: true
+
+  validates :grade, format: {with: /(?<=^|[\s,])(?:[A-D][-+]?|F)(?=[-+.]\B|[\s,]|$)/i , message: 'only allow american grades (A,B,C,D,F) '}
+
   belongs_to :user
 end
