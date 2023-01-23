3fe0980b8ec2465f11e1455a15cf0368d30f44c0
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-23 16:44:26 +0100

    More validation for grade points.

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -4,6 +4,8 @@ class Grade < ApplicationRecord
 
   validates_uniqueness_of :code, :course, scope: :user_id
 
+  validates :points ,:numericality => true
+
   validates :grade, format: {with: /(?<=^|[\s,])(?:[A-D][-+]?|F)(?=[-+.]\B|[\s,]|$)/i , message: 'only allow american grades (A,B,C,D,F) '}
 
   belongs_to :user
