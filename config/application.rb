5be9cb374b6408dcb652b99e665abce94a98f022
Haris Avdicevic <haris.avdi93@gmail.com>
2023-01-25 08:03:39 +0100

    Fixed time zone to Stockholm time.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module EcDemo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.time_zone = "Stockholm"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
